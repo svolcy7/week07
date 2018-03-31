5cb940c6fe3276945888fe435e09d46b63d122c2
samson m volcy <smv36@afsconnect2.njit.edu>
2018-03-30 23:51:22 -0400

    new commit

--- show.php
+++ show.php
@@ -1,16 +1,19 @@
 <?php
 require 'db.php' ;
-$sql = "select id,email,fname,password from smv36.accounts  ";
+$sql = "select id,email,fname,lname,password from smv36.accounts  ";
 
 $results=runQuery($sql);
 if(count($results) > 0)
 {
-	echo "<table border=\"1\"><tr><th>ID</th><th>Email</th><th>First Name</th><th>Pass</th></tr>";
+	echo "<table border=\"3
+	\"><tr><th>ID</th><th>Email</th><th>First Name</th><th>Last Name</th><th>Phone</th><th>Birthday</th><th>Pass</th></tr>";
 	foreach ($results as $row) {
-		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["password"]."</td></tr>";
+		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["phone"]."</td><td>".$row["birthday"]."</td><td>".$row["password"]."</td></tr>";
 	}
 	
-}else{
+}
+
+else{
     echo '0 results';
 }
 ?>
